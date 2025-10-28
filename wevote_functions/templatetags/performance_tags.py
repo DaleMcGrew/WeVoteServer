af5e6bb15ad039964a5bbef3dcc7847eae2968c5
Ricardo Reynoso <ricardo.reynoso102@gmail.com>
2025-10-28 14:38:26 -0700

    [WV-2135] Split up totals for different funcs in speed_statistics_banner

--- wevote_functions/templatetags/performance_tags.py
+++ wevote_functions/templatetags/performance_tags.py
@@ -3,10 +3,14 @@
 # -*- coding: UTF-8 -*-
 
 from django import template
-from wevote_functions.functions import get_performance_total
 
 register = template.Library()
 
 @register.filter
 def performance_total(performance_dict):
-    return get_performance_total(performance_dict)
+    total = 0
+    for time in performance_dict:
+        time_diff = time.get('time_difference') if isinstance(time, dict) else getattr(time, 'time_difference', 0)
+        if time_diff:
+            total += time_diff
+    return total
