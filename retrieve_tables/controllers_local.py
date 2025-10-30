62dcea505518f956f9f2326e46349b66dc444d30
Marcel <marcel.m.jacquot@gmail.com>
2025-10-29 23:43:22 -0700

    Added 1mb chunk processing

--- retrieve_tables/controllers_local.py
+++ retrieve_tables/controllers_local.py
@@ -123,7 +123,8 @@ def restore_one_file_to_local_server(aws_s3_file_url, table_name):
         tf = tempfile.NamedTemporaryFile(mode='r+b')
         with requests.get(aws_s3_file_url, stream=True) as response:
             response.raise_for_status()
-            for chunk in response.iter_content(chunk_size=100000):
+            # Process in 1MB chunks
+            for chunk in response.iter_content(chunk_size=(1000*1000)):
                 if chunk:
                     tf.write(chunk)
         tf.close()
