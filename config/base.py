34bf767257fa0007fd79c238b1ec62e6521ca59e
stevepodell <steve@podell.com>
2026-01-23 14:08:34 -0800

    CORS update for iOS app and POST APIs without @csrf_exempt

--- config/base.py
+++ config/base.py
@@ -99,6 +99,7 @@ ALLOWED_HOSTS = [
     'api.wevoteusa.org',
     'localhost',
     'wevotedeveloper.com',
+    'app://localhost',
     '127.0.0.1'
 ]
 
@@ -352,7 +353,7 @@ CORS_ALLOW_CREDENTIALS = True
 # CORS_REPLACE_HTTPS_REFERER = True
 CSRF_TRUSTED_ORIGINS = [
     'https://api.wevoteusa.org',
-    'http://localhost:8000', 'https://localhost:8000',
+    'http://localhost:8000', 'https://localhost:8000', 'app://localhost',
     'http://wevotedeveloper.com', 'https://wevotedeveloper.com',
 ]
 DATA_UPLOAD_MAX_MEMORY_SIZE = 22000000  # MAX_IMAGE_SIZE (room for 21MB file), but this does not seem to have any effect
@@ -360,7 +361,7 @@ DATA_UPLOAD_MAX_NUMBER_FIELDS = 4096
 
 CORS_ORIGIN_WHITELIST = (
     'https://api.wevoteusa.org',
-    'http://localhost:8000', 'https://localhost:8000',
+    'http://localhost:8000', 'https://localhost:8000', 'app://localhost'
     'http://wevotedeveloper.com', 'https://wevotedeveloper.com',
 )
 # CORS_ALLOW_HEADERS = (
